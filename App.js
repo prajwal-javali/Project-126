63bbd548af6a682708dc7719f3e429d13ece52b3
Prajwal Javali <73768092+prajwal-javali@users.noreply.github.com>
2022-09-21 16:31:33 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React , {Component} from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+import Camera from './screens/camera';
+
+export default class App extends Component {
+  render(){
+  return (
+
+      <Camera/>
+     
+  );
+  }
+}
